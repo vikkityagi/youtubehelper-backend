2c5260ae5318afc12c7a5bc3cba33e911b43ee85
vikki <Vikram.singh85@nic.in>
2026-03-11 14:17:52 +0530

    added some file

--- ai_search.py
+++ ai_search.py
@@ -1,55 +1,15 @@
-# from sentence_transformers import SentenceTransformer
-# import json
-
-# model = SentenceTransformer('all-MiniLM-L6-v2')
-# # model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
-
-# with open("video_data.json") as f:
-#     videos = json.load(f)
-
-# video_vectors = [model.encode(v["text"]) for v in videos]
-
-# def search_video(query):
-#     query_vector = model.encode(query)
-
-#     best_index = 0
-#     best_score = 0
-
-#     for i, vector in enumerate(video_vectors):
-#         score = sum(query_vector * vector)
-#         if score > best_score:
-#             best_score = score
-#             best_index = i
-
-#     return videos[best_index]
 from sentence_transformers import SentenceTransformer
 import json
 
-model = None
-videos = None
-video_vectors = None
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('paraphrase-multilingual-MiniLM-L12-v2')
 
+with open("video_data.json") as f:
+    videos = json.load(f)
 
-def load_resources():
-    global model, videos, video_vectors
-
-    if model is None:
-        print("Loading AI model...")
-        model = SentenceTransformer("all-MiniLM-L6-v2")
-
-    if videos is None:
-        print("Loading video data...")
-        with open("video_data.json") as f:
-            videos = json.load(f)
-
-    if video_vectors is None:
-        print("Encoding videos...")
-        video_vectors = [model.encode(v["text"]) for v in videos]
-
+video_vectors = [model.encode(v["text"]) for v in videos]
 
 def search_video(query):
-    load_resources()
-
     query_vector = model.encode(query)
 
     best_index = 0
@@ -62,3 +22,43 @@ def search_video(query):
             best_index = i
 
     return videos[best_index]
+# from sentence_transformers import SentenceTransformer
+# import json
+
+# model = None
+# videos = None
+# video_vectors = None
+
+
+# def load_resources():
+#     global model, videos, video_vectors
+
+#     if model is None:
+#         print("Loading AI model...")
+#         model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
+
+#     if videos is None:
+#         print("Loading video data...")
+#         with open("video_data.json") as f:
+#             videos = json.load(f)
+
+#     if video_vectors is None:
+#         print("Encoding videos...")
+#         video_vectors = [model.encode(v["text"]) for v in videos]
+
+
+# def search_video(query):
+#     load_resources()
+
+#     query_vector = model.encode(query)
+
+#     best_index = 0
+#     best_score = 0
+
+#     for i, vector in enumerate(video_vectors):
+#         score = sum(query_vector * vector)
+#         if score > best_score:
+#             best_score = score
+#             best_index = i
+
+#     return videos[best_index]
